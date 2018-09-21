f30736a55d896510a5e1d07564e4ec70533aee02
Leonardo Lima <leonardoresendelima@gmail.com>
2018-09-20 22:46:04 -0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'interceptor';
+	title = 'interceptor';
+
+	constructor(private httpClient: HttpClient) { }
+
+	get() {
+		this.httpClient.get("https://jsonplaceholder.typicode.com/users").subscribe(
+			(data: HttpResponse<any>) => {
+				console.log(data);
+			},
+			(error: HttpErrorResponse) => {
+				console.log(error);
+			}
+		)
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthInterceptor } from './auth.interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		HttpClientModule
+	],
+	providers: [
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: AuthInterceptor,
+			multi: true
+		},
+
+	],
+	bootstrap: [
+		AppComponent,
+
+	]
 })
 export class AppModule { }
